9e794c294338b29ec9487854023a1b83bb88616c
Daniel Kingery <danielkingery0@gmail.com>
2024-09-29 20:28:42 -0600

    fix(auth test) add testUserId

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -3,11 +3,13 @@ const app = require('../service');
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
+let testUserId;
 
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
   const registerRes = await request(app).post('/api/auth').send(testUser);
   testUserAuthToken = registerRes.body.token;
+    testUserId = registerRes.body.user.id;
 });
 
 test('login', async () => {
