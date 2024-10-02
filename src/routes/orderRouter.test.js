ae41a8a71fef57f9b672b87eb7912efb4b537364
Daniel Kingery <danielkingery0@gmail.com>
2024-10-01 20:13:53 -0600

    fix(order test) add menu item for get menu

--- src/routes/orderRouter.test.js
+++ src/routes/orderRouter.test.js
@@ -1,5 +1,57 @@
 const request = require('supertest');
 const app = require('../service');
+const { Role, DB } = require('../database/database.js');
+
+async function createAdminUser() {
+    let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+    user.name = randomName();
+    user.email = user.name + '@admin.com';
+
+    await DB.addUser(user);
+
+    user.password = 'toomanysecrets';
+    return user;
+}
+
+function randomName() {
+    return Math.random().toString(36).substring(2, 12);
+}
+
+function randomPrice() {
+    return (Math.random() * 100).toFixed(2);
+}
+
+async function createMenuItem() {
+    let title = randomName();
+    let menuItem = {
+        title: title,
+        description: randomName(),
+        image: title + '.png',
+        price: randomPrice(),
+    };
+
+    const menuItemRes = await request(app)
+        .put('/api/order/menu')
+        .set('Authorization', `Bearer ${adminUser.token}`)
+        .send(menuItem);
+
+    let fullMenu = menuItemRes.body;
+
+    return fullMenu;
+}
+
+let adminUser;
+let fullMenu;
+
+beforeAll(async () => {
+    adminUser = await createAdminUser();
+    const loginRes = await request(app).put('/api/auth').send(adminUser);
+
+    adminUser.id = loginRes.body.user.id;
+    adminUser.token = loginRes.body.token;
+
+    fullMenu = await createMenuItem();
+});
 
 test('get menu', async () => {
     const menuRes = await request(app).get('/api/order/menu').send();
@@ -7,11 +59,5 @@ test('get menu', async () => {
 
     const menu = menuRes.body;
 
-    expect(menu).toContainEqual({
-        id: 1,
-        title: 'Veggie',
-        image: 'pizza1.png',
-        price: 0.0038,
-        description: 'A garden of delight',
-    });
+    expect(menu).toEqual(fullMenu);
 });
