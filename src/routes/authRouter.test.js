e81d21aeb7bae19cd9eab7cdf5b663d20aaeb1d7
Daniel Kingery <danielkingery0@gmail.com>
2024-11-26 14:36:41 -0700

    remove(test) logging

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -6,54 +6,50 @@ let testUserAuthToken;
 let testUserId;
 
 beforeEach(async () => {
-    testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
-    const registerRes = await request(app).post('/api/auth').send(testUser);
-    testUserAuthToken = registerRes.body.token;
-    testUserId = registerRes.body.user.id;
+  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  testUserAuthToken = registerRes.body.token;
+  testUserId = registerRes.body.user.id;
 });
 
 test('login', async () => {
-    const loginRes = await request(app).put('/api/auth').send(testUser);
-    expect(loginRes.status).toBe(200);
-    expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+  const loginRes = await request(app).put('/api/auth').send(testUser);
+  expect(loginRes.status).toBe(200);
+  expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
 
-    const user = {
-        name: testUser.name,
-        email: testUser.email,
-        roles: [{ role: 'diner' }],
-    };
-    expect(loginRes.body.user).toMatchObject(user);
+  const user = {
+    name: testUser.name,
+    email: testUser.email,
+    roles: [{ role: 'diner' }],
+  };
+  expect(loginRes.body.user).toMatchObject(user);
 });
 
 test('logout', async () => {
-    const logoutRes = await request(app)
-        .delete('/api/auth')
-        .auth(testUserAuthToken, { type: 'bearer' });
-    expect(logoutRes.status).toBe(200);
-    expect(logoutRes.body.message).toBe('logout successful');
+  const logoutRes = await request(app)
+    .delete('/api/auth')
+    .auth(testUserAuthToken, { type: 'bearer' });
+  expect(logoutRes.status).toBe(200);
+  expect(logoutRes.body.message).toBe('logout successful');
 });
 
 test('update user', async () => {
-    console.log(testUserId);
-    console.log(testUserAuthToken);
-    const testEmail = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const testEmail = Math.random().toString(36).substring(2, 12) + '@test.com';
 
-    const updateUserRes = await request(app)
-        .put(`/api/auth/${testUserId}`)
-        .auth(testUserAuthToken, { type: 'bearer' })
-        .send({
-            email: testEmail,
-            password: 'b',
-        });
+  const updateUserRes = await request(app)
+    .put(`/api/auth/${testUserId}`)
+    .auth(testUserAuthToken, { type: 'bearer' })
+    .send({
+      email: testEmail,
+      password: 'b',
+    });
 
-    //   console.log(updateUserRes.body);
-
-    expect(updateUserRes.status).toBe(200);
-    expect(updateUserRes.body.email).toBe(testEmail);
+  expect(updateUserRes.status).toBe(200);
+  expect(updateUserRes.body.email).toBe(testEmail);
 });
 
 test('register error', async () => {
-    const registerRes = await request(app).post('/api/auth').send({});
-    expect(registerRes.status).toBe(400);
-    expect(registerRes.body.message).toBe('name, email, and password are required');
+  const registerRes = await request(app).post('/api/auth').send({});
+  expect(registerRes.status).toBe(400);
+  expect(registerRes.body.message).toBe('name, email, and password are required');
 });
